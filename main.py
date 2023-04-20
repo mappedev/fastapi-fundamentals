744c0b1b67a9dd5a9dfbc2ca61d59a31dca717bf
mappedev <mappedev@gmail.com>
2023-04-20 12:23:00 -0400

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+''' Python imports '''
+from typing import Optional
+
+''' Pydantic imports '''
+from pydantic import BaseModel
+
+''' FastAPI imports '''
+from fastapi import Body, FastAPI
 
 app = FastAPI()
+
+
+''' Models '''
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 @app.get('/')
 def home():
     return {'Hello': 'World'}
+
+''' Request and response body '''
+@app.post('/persons')
+def create_person(person: Person = Body(...)):
+    return person
