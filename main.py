521d26cd1aa95a689d216226abf30bf188be4baa
mappedev <mappedev@gmail.com>
2023-04-20 18:57:28 -0400

    validations_query

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 ''' FastAPI imports '''
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Query
 
 app = FastAPI()
 
@@ -27,3 +27,22 @@ def home():
 @app.post('/persons')
 def create_person(person: Person = Body(...)):
     return person
+
+''' Validations query parameters '''
+@app.get('/persons')
+def get_persons(
+    name: Optional[str] = Query(
+        default='Anonymous',
+        min_length=1,
+        max_length=50,
+        title='Person name',
+        description= 'Person name (Mario)',
+    ),
+    age: int = Query(
+        ..., # required
+        ge=0,
+        title='Person age',
+        description='Person age (21)',
+    ),
+):
+    return {name: age}
